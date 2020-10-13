a5b4b4268a131ec3991f94c5e3f353508ea2e487
venus_mae <veemaee@gmail.com>
2020-10-12 22:39:27 -0700

    Added an array of Harry Potter characters to js file. Populated the array with 4 objects, each with the same keys, different values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let harryPotterCharacters = [
+    {name: 'Harry Potter', height: 6, types: ['Gryffindor', 'Wizard'], blood_status: 'Half-blood'},
+    {name: 'Hermione Jean Granger', height: 5, types: ['Gryffindor', 'Witch'], blood_status: 'Muggle-born'},
+    {name: 'Ron Weasley', height: 6, types: ['Gryffindor', 'Wizard'], blood_status: 'Pure blood'},
+    {name: 'Luna Lovegood', height: 5, types: ['Ravenclaw', 'Witch'], blood_status: 'Pure blood'}
+];
