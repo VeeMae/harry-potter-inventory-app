b53283b67233bb6710c0d797171b49352fac2534
venus_mae <veemaee@gmail.com>
2020-10-14 00:07:18 -0700

    Changed the if-else statement and comments of the js file to better display the group consisting of the tallest character and its special message

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,15 @@ let harryPotterCharacters = [
     {name: 'Luna Lovegood', height: 5.5, types: ['Ravenclaw', 'Witch'], blood_status: 'Pure blood'}
 ];
 
-let specialMessage = "Amazing, you're mighty tall! Well done, now you can reach the top cupboards just fine without magic!";
-
 for (let i = 0; i <harryPotterCharacters.length; i++) {
+     // Prints the name, blood status, and height of each Harry Potter character from the array. Adds a break after each print.
+    document.write(`${harryPotterCharacters[i].name} | Blood-Status: ${harryPotterCharacters[i].blood_status} | Height: ${harryPotterCharacters[i].height} <br>`);
     if (harryPotterCharacters[i].height <= 5.7) {
-        document.write(`${harryPotterCharacters[i].name} | Height: ${harryPotterCharacters[i].height} | Blood-Status: ${harryPotterCharacters[i].blood_status} <br> <br> `);
-        // Prints the name of each Harry Potter character from the array and their heights. Adds a break after each print.
+        // Adds another break after each character if less than 5.7. This allows me to group together the characters who are taller than 5.7 with their special message. 
+        document.write(`<br>`);
     }else if (harryPotterCharacters[i].height > 5.7) {
-        document.write(`${harryPotterCharacters[i].name} | Height: ${harryPotterCharacters[i].height} - ${specialMessage} | Blood-Status: ${harryPotterCharacters[i].blood_status} <br> <br>`)
-        // Prints out the statement if the character is taller than 5.7. Adds a break after each print.
+        let specialMessage = `Amazing ${harryPotterCharacters[i].name}, you're mighty tall! Well done, now you can reach the top cupboards just fine without magic!`;
+         // Adds a special message to character if taller 5.7. Also adds a break after each print
+        document.write(`- ${specialMessage} - <br> <br>`)
     }
 }
