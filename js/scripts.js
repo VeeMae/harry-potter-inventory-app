a12464d51d062327e4f2acb314625a4aae686c25
venus_mae <veemaee@gmail.com>
2020-10-18 20:57:56 -0700

    Wrap the harryPotterCharacters array in an IIFE, and include public functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
-let harryPotterCharacters = [
+let harryPotterRepository = (function () {
+    let harryPotterCharacters = [
     {name: 'Harry Potter', height: 5.7, types: ['Gryffindor', 'Wizard'], blood_status: 'Half-blood'},
     {name: 'Hermione Jean Granger', height: 5.5, types: ['Gryffindor', 'Witch'], blood_status: 'Muggle-born'},
     {name: 'Ron Weasley', height: 5.9, types: ['Gryffindor', 'Wizard'], blood_status: 'Pure blood'},
     {name: 'Luna Lovegood', height: 5.5, types: ['Ravenclaw', 'Witch'], blood_status: 'Pure blood'}
 ];
+    function add(hpCharacter) {
+        harryPotterCharacters.push(hpCharacter);
+    }
+    function getAll() {
+        return harryPotterCharacters;
+    }
 
-harryPotterCharacters.forEach(function (character) {
+    return {
+        add: add,
+        getAll: getAll
+    };
+}) ();
+
+harryPotterRepository.getAll().forEach( function (character) {
     document.write("<p>" + character.name + " is " + character.height + " feet tall," + " of the " + character.blood_status + " type, " + "and resides in the " + character.types[0] + " house." + "</p>")
 })
